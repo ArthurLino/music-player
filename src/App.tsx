e6f03c84075af615023c0d2874c62eef539281f3
Arthur Lino <arthur.ssLino@gmail.com>
2023-01-16 13:46:13 -0300

    🎉 style: added styles
    added: cta links to github and linkedin
    added: button images
    fix: changed the link-through font import to sass import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import { createRef, useCallback, useEffect, useState } from "react";
-import musicPath from "./assets/musics/1.mp3";
+import { createRef, useEffect, useState } from "react";
+
 import musicBanner from "./assets/images/1.jpeg";
+import musicPath from "./assets/musics/1.mp3";
+
+import fastBackwardIcon from "./assets/images/backward.svg";
+import fastForwardIcon from "./assets/images/forward.svg";
+import playIcon from "./assets/images/play.svg";
+import pauseIcon from "./assets/images/pause.svg";
+import githubIcon from "./assets/images/github.svg";
+import linkedinIcon from "./assets/images/linkedin.svg";
+
+import "./styles/global.scss";
+import "./styles/app.scss";
 
 function App() {
   const audioRef = createRef<HTMLAudioElement>();
@@ -22,86 +33,116 @@ function App() {
   useEffect(() => {
     isPlaying ? audioRef.current?.play() : audioRef.current?.pause();
     setProgressTime(musicCurrentTime);
+    progressRef.current?.style.setProperty(
+      "--background-size",
+      `${(musicCurrentTime / musicDuration) * 100}%`
+    );
   }, [isPlaying, musicCurrentTime]);
 
   return (
     <div className="App">
-      <main>
-        <div className="player">
-          <img src={musicBanner} alt="" />
-          <h3>Os Anjos Te Louvam</h3>
-          <h4>Eli Soares</h4>
-          <audio
-            onTimeUpdate={() => {
-              if (audioRef.current) {
-                setMusicCurrentTime(Math.floor(audioRef.current.currentTime));
-              }
-            }}
-            onCanPlay={() => {
-              if (audioRef.current)
-                setMusicDuration(Math.floor(audioRef.current.duration));
-            }}
-            ref={audioRef}
-            src={currentMusic}
-          />
-          <div className="controller">
-            <div>
-              <button
-                onClick={() => {
-                  if (audioRef.current) {
-                    audioRef.current.currentTime = musicCurrentTime - 5;
-                  }
-                }}
-              >
-                Back
-              </button>
-              <button
-                onClick={() => {
-                  setIsPlaying(!isPlaying);
-                }}
-              >
-                Play/Pause
-              </button>
-              <button
-                onClick={() => {
-                  if (audioRef.current) {
-                    audioRef.current.currentTime = musicCurrentTime + 5;
-                  }
-                }}
-              >
-                Next
-              </button>
-            </div>
-            <input
-              className="player-progress"
-              type="range"
-              step={1}
-              min={0}
-              max={musicDuration}
-              onMouseUp={() => {
+      <main className="player">
+        <img src={musicBanner} alt="" />
+        <div className="player-text">
+          <h1>Os Anjos Te Louvam</h1>
+          <h2>Eli Soares</h2>
+        </div>
+        <audio
+          onEnded={() => {
+            if (audioRef.current) {
+              setIsPlaying(false);
+              audioRef.current.currentTime = 0;
+            }
+          }}
+          onTimeUpdate={() => {
+            if (audioRef.current) {
+              setMusicCurrentTime(Math.floor(audioRef.current.currentTime));
+            }
+          }}
+          onCanPlay={() => {
+            if (audioRef.current)
+              setMusicDuration(Math.floor(audioRef.current.duration));
+          }}
+          ref={audioRef}
+          src={currentMusic}
+        />
+        <div className="player-controllers">
+          <div className="buttons">
+            <button
+              onClick={() => {
                 if (audioRef.current) {
-                  audioRef.current.currentTime = progressTime;
-                  setMusicCurrentTime(progressTime);
+                  audioRef.current.currentTime = musicCurrentTime - 5;
                 }
-                setIsPlaying(true);
               }}
-              onMouseDown={() => {
-                setIsPlaying(false);
+            >
+              <img src={fastBackwardIcon} alt="" />
+            </button>
+            <button
+              onClick={() => {
+                setIsPlaying(!isPlaying);
               }}
-              onChange={(e) => {
-                console.log(Number(e.target.value));
-                setProgressTime(Number(e.target.value));
+            >
+              {isPlaying ? (
+                <img src={pauseIcon} alt="" />
+              ) : (
+                <img src={playIcon} alt="" />
+              )}
+            </button>
+            <button
+              onClick={() => {
+                if (audioRef.current) {
+                  audioRef.current.currentTime = musicCurrentTime + 5;
+                }
               }}
-              value={progressTime}
-              ref={progressRef}
-            />
-            <div className="player-time">
-              <span>{formatMinutesAndSeconds(musicCurrentTime)}</span>
-              <span>{formatMinutesAndSeconds(musicDuration)}</span>
-            </div>
+            >
+              <img src={fastForwardIcon} alt="" />
+            </button>
+          </div>
+          <input
+            className="player-progress"
+            type="range"
+            step={1}
+            min={0}
+            max={musicDuration}
+            onMouseUp={() => {
+              if (audioRef.current) {
+                audioRef.current.currentTime = progressTime;
+                setMusicCurrentTime(progressTime);
+              }
+              setIsPlaying(true);
+            }}
+            onMouseDown={() => {
+              setIsPlaying(false);
+            }}
+            onChange={(e) => {
+              setProgressTime(Number(e.target.value));
+            }}
+            value={progressTime}
+            ref={progressRef}
+          />
+          <div className="player-time">
+            <span>{formatMinutesAndSeconds(musicCurrentTime)}</span>
+            <span>{formatMinutesAndSeconds(musicDuration)}</span>
           </div>
         </div>
       </main>
+      <footer>
+        Da uma olhada:
+        <div>
+          <a
+            href="https://github.com/ArthurLino/music-player/blob/main/README.md"
+            target="_blank"
+          >
+            <img src={githubIcon} alt="" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/arthur-lino-silva-371a10206/"
+            target="_blank"
+          >
+            <img src={linkedinIcon} alt="" />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
